90faaa4a4962dd215bfc23d46336891aa224b736
Erik <eriknikoyan555@yandex.ru>
2024-02-07 22:30:44 +0300

    Aside-menu-adaptive

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-/* Hide and show aside menu */
-const toggleBtn = document.querySelector('.aside-menu-toggle');
+/* Hide and Show aside menu */
+const button = document.querySelector('.aside-menu-toggle');
 const targetElement = document.querySelector(".aside");
 
-const toggleAsideMenu = () => {
+button.addEventListener('click', () => {
   if (targetElement.classList.contains('aside_closed')) {
     targetElement.classList.remove('aside_closed');
   } else {
     targetElement.classList.add('aside_closed');
   }
-}
-toggleBtn.addEventListener('click', toggleAsideMenu);
-const checkScreenWidth = () => {
-  if (window.innerWidth < 998) {
-    targetElement.classList.add('aside_closed');
-  }
-}
-window.addEventListener('resize', checkScreenWidth);
-checkScreenWidth();
+});
+// toggleBtn.addEventListener('click', toggleAsideMenu);
+// const checkScreenWidth = () => {
+//   if (window.innerWidth < 768) {
+//     targetElement.classList.add('aside_closed');
+//   }
+// }
+// window.addEventListener('resize', checkScreenWidth);
+// checkScreenWidth();
