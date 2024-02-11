9a83646f18d025713ff7823dc894510afa72053d
Erik <eriknikoyan555@yandex.ru>
2024-02-11 12:10:41 +0300

    Student-Page-js-fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 /* Hide and Show aside menu */
-const button = document.querySelector('.aside-menu-toggle');
+const button = document.querySelector(".aside-menu-toggle");
 const targetElement = document.querySelector(".aside");
 
-button.addEventListener('click', () => {
-  if (targetElement.classList.contains('aside_closed')) {
-    targetElement.classList.remove('aside_closed');
+button.addEventListener("click", () => {
+  if (targetElement.classList.contains("aside_closed")) {
+    targetElement.classList.remove("aside_closed");
   } else {
-    targetElement.classList.add('aside_closed');
+    targetElement.classList.add("aside_closed");
   }
 });
