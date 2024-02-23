29a0eb0426744ab13e6e326919b968b159656cf8
Erik <eriknikoyan555@yandex.ru>
2024-02-23 17:04:50 +0300

    Auth-MVC-body

---
+++ EdManagementSystem.App/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EdManagementSystem.App.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Route("[controller]/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("[controller]/recovery-password")]
+        public IActionResult RecoveryPassword()
+        {
+            return View();
+        }
+    }
+}
