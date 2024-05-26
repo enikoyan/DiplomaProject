1f1fb7ce09c65635348b1ebdc3ffe550bca2e68d
Erik <eriknikoyan555@yandex.ru>
2024-05-26 13:48:21 +0300

    ChartsFix

--- EdManagementSystem.App/wwwroot/js/main.js
+++ EdManagementSystem.App/wwwroot/js/main.js
@@ -11,7 +11,38 @@ const logoutForm = document.querySelector('form[action="/Auth/Logout"]');
 const logoutItem = document.getElementById("logoutItem");
 const logoutAPIUri = "https://localhost:44354/auth/Logout";
 
+// Preloader
+var preloader = document.querySelector(".preloader");
+var lottieLogo = document.getElementById("logoLottie");
+var links = document.querySelectorAll("a");
+
+const animation = lottie.loadAnimation({
+    container: document.getElementById("logoLottie"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "../js/outerJS/lottie_logo.json",
+});
+
 window.addEventListener("DOMContentLoaded", async () => {
+
+    // Preloader handler
+    links.forEach(function (link) {
+        link.addEventListener("click", function () {
+            setTimeout(function () {
+                preloader.classList.remove("preloader_hidden");
+                lottieLogo.classList.remove("logoLottie_hidden");
+            }, 800);
+        });
+    });
+
+    window.addEventListener("load", function () {
+        setTimeout(function () {
+            preloader.classList.add("preloader_hidden");
+            lottieLogo.classList.add("logoLottie_hidden");
+        }, 1000);
+    });
+
     // Logout handler
     logOutBtn.addEventListener("click", () => {
         localStorage.clear();
@@ -58,34 +89,4 @@ window.addEventListener("DOMContentLoaded", async () => {
             localStorage.setItem("sidebarState", false);
         }
     }
-});
-
-// PRELOADER
-const animation = lottie.loadAnimation({
-    container: document.getElementById("logoLottie"),
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: "../js/outerJS/lottie_logo.json",
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    var preloader = document.querySelector(".preloader");
-    var lottieLogo = document.getElementById("logoLottie");
-    var links = document.querySelectorAll("a");
-
-    links.forEach(function (link) {
-        link.addEventListener("click", function () {
-            setTimeout(function () {
-                preloader.classList.remove("preloader_hidden");
-                lottieLogo.classList.remove("logoLottie_hidden");
-            }, 200);
-        });
-    });
-    window.addEventListener("load", function () {
-        setTimeout(function () {
-            preloader.classList.add("preloader_hidden");
-            lottieLogo.classList.add("logoLottie_hidden");
-        }, 600);
-    });
 });
